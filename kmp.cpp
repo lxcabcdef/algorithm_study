9bd7c19ee1572131332711b2e38695d08f633c44
lxcabcdef <34327016+lxcabcdef@users.noreply.github.com>
2023-11-11 16:18:43 +0800

    Format kmp cpp code

--- kmp.cpp
+++ kmp.cpp
@@ -1,14 +1,15 @@
-#include<string>
+#include <string>
 
-class Kmp{
+class Kmp {
 public:
-    Kmp(std::string key) : key_(key) {
+    Kmp(std::string key) : key_(key)
+    {
         next_ = new int[key.size()];
         next_[0] = 0;
-        int i,j;
+        int i, j;
         for (i = 1, j = 0; i < key.size(); i++) {
-            while (j>0 && key[i] != key[j]) {
-                j = next_[j-1];
+            while (j > 0 && key[i] != key[j]) {
+                j = next_[j - 1];
             }
             if (key[i] == key[j]) {
                 j++;
@@ -16,24 +17,27 @@ public:
             next_[i] = j;
         }
     }
-    ~Kmp() {
+    ~Kmp()
+    {
         delete next_;
     }
 
-    int search(std::string txt) {
-        int i,j;
+    int search(std::string txt)
+    {
+        int i, j;
         for (i = 0, j = 0; i < txt.size(); i++) {
             while (txt[i] != key_[j] && j != 0) {
                 j = next_[j];
             }
             j++;
-            if (j == key_.size()) 
+            if (j == key_.size())
                 break;
         }
         i = i == txt.size() ? -1 : i - key_.size() + 2;
         return i;
     }
+
 private:
     std::string key_;
-    int *next_;
+    int* next_;
 };
